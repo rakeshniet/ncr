8b9523f7d275784f1a8f5596ee7043971adb2969
Rakesh kumar <104338380+rakeshniet@users.noreply.github.com>
2026-05-26 22:38:38 +0530

    Add new resource group in central India
    Add new resource group in central India

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "test2" {
   name     = "rg1"
   location = "east us"
 }
+
+resource "azurerm_resource_group" "test3" {
+  name     = "rg1"
+  location = "central india"
+}
