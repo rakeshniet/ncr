f83545682e4481e5a3042e42ae7f9605ebd179c1
Rakesh kumar <104338380+rakeshniet@users.noreply.github.com>
2026-05-26 22:20:06 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,10 +1,4 @@
-variable "rgs" {}
-# variable "rg_map" {}
-
 resource "azurerm_resource_group" "test2" {
-    for_each = var.rgs
-
-  name     = each.value.rg_name
-  location = each.value.location
-  managed_by = each.value.managed_by
+  name     = "rg1"
+  location = "central india"
 }
